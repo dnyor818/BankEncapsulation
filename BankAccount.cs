d5355e82a0a0a3882a4e11bf3c72669a2b17b2e2
Your Name <you@example.com>
2020-11-03 11:28:43 -0600

    initial commit

---
+++ BankAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simuating a deposit {amount, 0:c} into balance");
+            _balance = amount;
+        }
+        
+        public double GetBalance()
+        {
+            return _balance;
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BankEncapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount ba = new BankAccount();
+            Console.WriteLine("How much would you like to deposit today?");
+            double amounttoDeposit = double.Parse(Console.ReadLine());
+            ba.Deposit(amounttoDeposit);
+            double userBalance = ba.GetBalance();
+            Console.WriteLine($"Your current balance is {userBalance, 0:c}");
+        }
+    }
+}
